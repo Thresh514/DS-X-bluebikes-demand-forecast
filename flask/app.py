7a6825476af3e73a4d97b346d6e85a111f6ea871
Thresh514 <165234885+Thresh514@users.noreply.github.com>
2025-10-25 17:49:40 -0400

    Update .gitignore and change model import in app.py
    - Added model.joblib and __pycache__/ to .gitignore.
    - Changed the import of the model in app.py from 'simulate_model' to 'test'.

--- flask/app.py
+++ flask/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 import pandas as pd
 from flask_cors import CORS
-from simulate_model import model
+from test import model
 
 app = Flask(__name__)
 CORS(app)  # 允许 Next.js 请求
